2a7c558db3460389f02afe2d6a45c8634270139b
nitin095 <32167005+nitin095@users.noreply.github.com>
2018-05-19 17:38:11 +0530

    Add files via upload

---
+++ script/data.js
@@ -0,0 +1,148 @@
+let users = [
+	{
+     	"id": 1,
+     	"name": "Person 1",
+     	"pictureUrl": "https://placeimg.com/101/101/people",
+     	"lastMessageDate": "today",
+     	"phone": 9999999999,
+     	"status": true,
+     	"online": true,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "09-05-2018"
+     	}
+ 	},
+     {
+     	"id": 2,
+     	"name": "Person 2",
+     	"pictureUrl": "https://placeimg.com/102/102/people",
+     	"lastMessageDate": "17-05-2018",
+     	"phone": 9999999002,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "Busy",
+     	    "updatedOn": "16-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 3,
+     	"name": "Person 3",
+     	"pictureUrl": "https://placeimg.com/103/103/people",
+     	"lastMessageDate": "today",
+     	"phone": 9999999003,
+     	"status": true,
+     	"online": false,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "29-04-2018"
+     	}
+ 	},
+     	{
+     	"id": 4,
+     	"name": "Person 4",
+     	"pictureUrl": "https://placeimg.com/104/104/people",
+     	"lastMessageDate": "16-05-2018",
+     	"phone": 9999999004,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "At work",
+     	    "updatedOn": "17-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 5,
+     	"name": "Person 5",
+     	"pictureUrl": "https://placeimg.com/105/105/people",
+     	"lastMessageDate": "15-05-2018",
+     	"phone": 9999999005,
+     	"status": false,
+     	"online": true,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "10-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 6,
+     	"name": "Person 6",
+     	"pictureUrl": "https://placeimg.com/106/106/people",
+     	"lastMessageDate": "today",
+     	"phone": 9999999006,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "11-03-2018"
+     	}
+ 	},
+     	{
+     	"id": 7,
+     	"name": "Person 7",
+     	"pictureUrl": "https://placeimg.com/107/107/people",
+     	"lastMessageDate": "15-05-2018",
+     	"phone": 9999999007,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "Busy",
+     	    "updatedOn": "17-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 8,
+     	"name": "Person 8",
+     	"pictureUrl": "https://placeimg.com/108/108/people",
+     	"lastMessageDate": "12-05-2018",
+     	"phone": 9999999008,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "09-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 9,
+     	"name": "Person 9",
+     	"pictureUrl": "https://placeimg.com/109/109/people",
+     	"lastMessageDate": "today",
+     	"phone": 9999999009,
+     	"status": true,
+     	"online": true,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "09-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 10,
+     	"name": "Person 10",
+     	"pictureUrl": "https://placeimg.com/110/110/people",
+     	"lastMessageDate": "14-05-2018",
+     	"phone": 9999999010,
+     	"status": false,
+     	"online": false,
+     	"about": {
+     	    "display": "At work",
+     	    "updatedOn": "17-05-2018"
+     	}
+ 	},
+     	{
+     	"id": 11,
+     	"name": "Person 11",
+     	"pictureUrl": "https://placeimg.com/111/111/people",
+     	"lastMessageDate": "today",
+     	"phone": 9999999011,
+     	"status": true,
+     	"online": false,
+     	"about": {
+     	    "display": "Available",
+     	    "updatedOn": "17-03-2018"
+     	}
+ 	}
+];
+
+
+
